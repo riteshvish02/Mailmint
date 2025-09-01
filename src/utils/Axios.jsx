576b288bae061778eb4155d224c2ccc179df9fcd
riteshvish02 <rv504263@gmail.com>
2025-09-01 12:59:07 +0530

    changed url bckend

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost'
-  ? 'http://localhost:5000'
+  ? 'http://localhost:3001'
   : 'https://wjpms.in';
 const Instance = axios.create({
     baseURL,
