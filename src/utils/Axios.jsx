42409b6f9717c95011b5d38b4bf1cbabd79cd2f8
riteshvish02 <rv504263@gmail.com>
2025-09-17 13:13:52 +0530

    fixed url

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost'
   ? 'http://localhost:3001'
-  : 'http://31.97.235.13/api';
+  : 'http://31.97.235.13';
 const Instance = axios.create({
     baseURL,
     withCredentials: true,
